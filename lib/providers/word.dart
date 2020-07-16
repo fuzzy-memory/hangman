8be04845e5d8f9fa9073ae3bb66c49f7616319c1
Tushar Machavolu <tush.machavolu@gmail.com>
2020-07-17 01:49:20 +0530

    Cleaned up code

--- lib/providers/word.dart
+++ lib/providers/word.dart
@@ -3,17 +3,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-class HTTPException implements Exception {
-  final String msg;
-
-  HTTPException(this.msg);
-
-  @override
-  String toString() {
-    return msg;
-  }
-}
-
 class Word with ChangeNotifier {
   String _obtainedWord, _blank = "_";
 
@@ -30,7 +19,6 @@ class Word with ChangeNotifier {
       _blank = "_";
       final url = "https://random-word-api.herokuapp.com//word?swear=0";
       final res = await http.get(url);
-      if (res.statusCode != 200) throw HTTPException(res.statusCode.toString());
       final resData = json.decode(res.body);
       _obtainedWord =
           resData.toString().substring(1, resData.toString().length - 1);
